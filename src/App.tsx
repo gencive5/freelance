910d6cc05b2970ea8fdc6e262ffbffe65861666a
gencive5 <vic.segen@gmail.com>
2025-05-11 18:11:17 +0200

    overflow x : hidden
    scroll oK

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const App = () => {
     <span aria-hidden="true">creative developer</span>
   </span>
 
+
   <em className="text-fit">
     <span><span>SERVICES: responsive development</span></span>
     <span aria-hidden="true">SERVICES: responsive development</span>
@@ -25,10 +26,27 @@ const App = () => {
     <span aria-hidden="true">DESIGN - UX/UI - BRAND IDENTITY</span>
   </em>
 
+  <em className="text-fit">
+    <span><span>based in Paris & available internationally</span></span>
+    <span aria-hidden="true">based in Paris & available internationally</span>
+  </em>
+
+    <em className="text-fit">
+    <span><span>available internationally</span></span>
+    <span aria-hidden="true">available internationally</span>
+  </em>
+
 </div>
+
+
     </div>
+    
   );
 };
 
 export default App;
 
+
+
+
+  
