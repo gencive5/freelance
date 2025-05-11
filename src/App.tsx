02365dcd095b3c653a4b8483056b61d373a476ec
gencive5 <vic.segen@gmail.com>
2025-05-11 17:06:11 +0200

    OK BACK TO LAYOUT actuaLLY
    text width fits

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 
-import './App.css'
-import InteractiveText from '../src/components/InteractiveText';
+import './App.css'; // Import the CSS file
 
-function App() {
-
-
+const App = () => {
   return (
-    <>
-     <div className="app-container">
-      <h1>Gencives</h1>
-      <InteractiveText />
+    <div className="container">
+      <div className="Figure-Demo">
+<p>
+  <span className="text-fit">
+   <span>What if I will tell you</span>
+    <span aria-hidden="true">What if I will tell you</span>
+  </span>
+  <span className="text-fit">
+    <span><span>how we could use</span></span>
+    <span aria-hidden="true">how we could use</span>
+  </span>
+  <em className="text-fit">
+    <span><span>registered custom properties</span></span>
+    <span aria-hidden="true">registered custom properties</span>
+  </em>
+  
+</p>
+</div>
     </div>
-     
-    </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
