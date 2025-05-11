125779d611de7b237efe8657830646d71c80b166
gencive5 <vic.segen@gmail.com>
2025-05-11 19:26:13 +0200

    separate component for text fit

---
+++ src/TextFit.tsx
@@ -0,0 +1,44 @@
+import './App.css';
+
+const TextFit = () => {
+  return (
+    <div className="Figure-Demo">
+      <span className="text-fit">
+        <span><span>gencives</span></span>
+        <span aria-hidden="true">gencives</span>
+      </span>
+      
+      <span className="text-fit">
+        <span><span>creative developer</span></span>
+        <span aria-hidden="true">creative developer</span>
+      </span>
+
+      <em className="text-fit">
+        <span><span>SERVICES: responsive development</span></span>
+        <span aria-hidden="true">SERVICES: responsive development</span>
+      </em>
+      
+      <em className="text-fit">
+        <span><span>DESIGN - UX/UI - BRAND IDENTITY</span></span>
+        <span aria-hidden="true">DESIGN - UX/UI - BRAND IDENTITY</span>
+      </em>
+
+      <em className="text-fit">
+        <span><span>based in Paris & available internationally</span></span>
+        <span aria-hidden="true">based in Paris & available internationally</span>
+      </em>
+
+      <em className="text-fit">
+        <span><span>For one landing page, count between 2 and 4 weeks of production.</span></span>
+        <span aria-hidden="true">For one landing page, count between 2 and 4 weeks of production.</span>
+      </em>
+
+      <em className="text-fit">
+        <span><span>A more elaborate website will take between 6 and 8 weeks</span></span>
+        <span aria-hidden="true">A more elaborate website will take between 6 and 8 weeks</span>
+      </em>
+    </div>
+  );
+};
+
+export default TextFit;
