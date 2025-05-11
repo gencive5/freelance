55e47404215e911c1a70cf933a16a3af1f7f00ea
gencive5 <vic.segen@gmail.com>
2025-05-11 19:37:05 +0200

    new component : Circles
    Decorative circles Vasarely style

---
+++ src/Circles.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+const Circles = () => {
+  return (
+
+<div className="container1">
+  <div className="circle"></div>
+  <div className="circle"></div>
+  <div className="circle"></div>
+  <div className="circle"></div>
+  <div className="circle"></div>
+</div>
+
+  )
+}
+
+export default Circles;
