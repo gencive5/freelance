0ae74bcb6ef4e2fe05c6517a95c9e1c0c747076f
gencive5 <vic.segen@gmail.com>
2025-06-11 22:08:07 +0200

    text okish

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,25 @@ const App = () => {
     <div className="container">
       <CircleGrid />
       <TextFit />
-      <div style={{ backgroundColor: 'red', padding: '20px' }}>ASK FOR YOUR WEBSITE NOW</div>
-      <p>Leave your contact details below and I will get back to you today</p>
+      <div style={{ backgroundColor: 'red', padding: '20px' }} className="Figure-Demo">
+      <span className="text-fit">
+        <span><span>ASK FOR YOUR WEBSITE NOW</span></span>
+        <span aria-hidden="true">ASK FOR YOUR WEBSITE NOW</span>
+      </span>
+      </div>
+       <div className="Figure-Demo">
+      <span className="text-fit">
+        <span><span>Leave your contact details below and I will get back to you today</span></span>
+        <span aria-hidden="true">Leave your contact details below and I will get back to you today</span>
+      </span>
+      </div>
       <ContactForm />
-      <p>or contact me at vic.segen@gmail.com</p>
+      <div className="Figure-Demo">
+      <span className="text-fit">
+        <span><span>or contact me at vic.segen@gmail.com</span></span>
+        <span aria-hidden="true">or contact me at vic.segen@gmail.com</span>
+      </span>
+      </div>
     </div>
   );
 };
