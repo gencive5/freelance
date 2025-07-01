d017cc219335fec88ae8626120410f5831a857af
gencive5 <vic.segen@gmail.com>
2025-07-01 22:32:33 +0200

    removed two last subtitles
    mobile version not ok

--- src/TextFit.jsx
+++ src/TextFit.jsx
@@ -15,24 +15,12 @@ const TextFit = () => {
       fr: "développeur créatif",
       es: "desarrollador creativo"
     },
-    location: {
-      en: "based in Paris & available internationally",
-      fr: "basé à Paris & disponible à l'international",
-      es: "basado en París & disponible internacionalmente"
-    },
-    deliver: {
-      en: "I DELiVER HiGH CONCEPT WEBSiTES",
-      fr: "JE LIVRE DES SITES WEB HAUT CONCEPT",
-      es: "CREO SITIOS WEB DE ALTO CONCEPTO"
-    },
   };
 
   return (
     <div className="textfit-container">
       <BlotterText text={texts.title[language]} initialSize={100} />
 <BlotterText text={texts.subtitle[language]} initialSize={60} />
-<BlotterText text={texts.location[language]} initialSize={50} />
-<BlotterText text={texts.deliver[language]} initialSize={20} />
     </div>
   );
 };
