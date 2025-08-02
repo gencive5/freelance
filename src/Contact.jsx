98cf13bfaba37a301f9c37c87c0c445dfbb9843e
gencive5 <vic.segen@gmail.com>
2025-08-02 15:09:42 +0200

    instagram btn css
    needs better layout

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,18 +1,12 @@
+import './App.css';
+
 const Contact = () => (
-  <footer className="fixed top-0 left-0 right-0 py-4 text-white text-base z-50 text-center bg-transparent">
-    <a
-      href="mailto:vic.segen@gmail.com"
-      className="mx-2 underline hover:opacity-80 transition-opacity"
-      style={{ color: '#1A73E8' }}
-    >
-      Email
-    </a>
-    |
+  <footer className="footer">
+  
     <a
       href="https://instagram.com/gencive5"
       target="_blank"
       rel="noopener noreferrer"
-      className="mx-2 underline hover:opacity-80 transition-opacity"
       style={{ color: '#ffffffff' }}
     >
       Instagram
