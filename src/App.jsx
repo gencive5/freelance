b0f0958e6c858babaaaef7a9b0c8287ee3771545
gencive5 <vic.segen@gmail.com>
2025-08-30 17:03:52 +0200

    opacity almost ok

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ const App = () => {
 
             <div className="Figure-Demo">
               <span className="text-fit">
-                <span><span>design -développement web</span></span>
+                <span><span>design - développement web</span></span>
                 <span aria-hidden="true">design - développement web</span>
               </span>
               <span className="text-fit">
