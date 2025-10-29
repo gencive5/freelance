3155bd45d98818284fec57c528d5476eb1ef6cb4
gencive5 <vic.segen@gmail.com>
2025-10-29 17:02:46 +0100

    contact links de part et d'autre

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,24 +2,27 @@ import './App.css';
 
 const Contact = () => (
   <footer className="footer">
-
-     <a
-      href="mailto:vic.segen@gmail.com"
-      className="link"
-      style={{ color: '#1A73E8' }}
-    >
-      Email
-    </a>
-  
-    <a
-      href="https://instagram.com/gencive5"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="link"
-      style={{ color: '#5a2adeff' }}
-    >
-      Instagram
-    </a>
+    <div className="footer-left">
+      <a
+        href="mailto:vic.segen@gmail.com"
+        className="link"
+        style={{ color: '#1A73E8' }}
+      >
+        Email
+      </a>
+    </div>
+    
+    <div className="footer-right">
+      <a
+        href="https://instagram.com/gencive5"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="link"
+        style={{ color: '#5a2adeff' }}
+      >
+        Instagram
+      </a>
+    </div>
   </footer>
 );
 
